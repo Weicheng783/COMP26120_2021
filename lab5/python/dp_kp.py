d0bec9564674e8ccf636dd86a3a9f4b62f38d631
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2022-01-18 21:03:28 +0000

    Dynamic programming solution

--- lab5/python/dp_kp.py
+++ lab5/python/dp_kp.py
@@ -24,20 +24,42 @@ class dp(knapsack):
         
         # V and Keep should be 2d arrays for use in the dynamic programming solution
         # The are both of size (n + 1)*(W + 1)
-        
+
         # Initialise V and keep
-        # ADD CODE HERE
-        
+        V = []
+        for i in range(0,n+1):
+            V.append([None]*(W+1))
+
+        Keep = []
+        for i in range(0,n+1):
+            Keep.append([None]*(W+1))
+
         # Set the values of the zeroth row of the partial solutions table to False
-        # ADD CODE HERE
-        
-        # main dynamic programming loops, adding on item at a time and looping through weights from 0 to W
-        # ADD CODE HERE
-        
-        # now discover which iterms were in the optimal solution
-        # ADD CODE HERE
-        
-        
+        for i in range(0,n+1):
+            V[0][i] = 0
+
+        # Main dynamic programming loops, adding on item at a time and looping through weights from 0 to W
+        for i in range(0,W+1):
+            V[0][i] = 0
+        for i in range(1,n+1):
+            for j in range(0,W+1):
+                if(wv[i] <= j and ((v[i] + V[i-1][j-wv[i]]) > V[i-1][j])):
+                    V[i][j] = v[i] + V[i-1][j-wv[i]]
+                    Keep[i][j] = 1
+                else:
+                    V[i][j] = V[i-1][j]
+                    Keep[i][j] = 0
+
+        # Now discover which items were in the optimal solution
+        k = W
+        cd = n
+        while(cd != 0):
+            if(Keep[cd][k] == True):
+                # print(str(cd)+" selected.")
+                solution[cd] = True
+                k = k - wv[cd]
+            cd -= 1
+
 knapsk = dp(sys.argv[1])
 solution = [False]*(knapsk.Nitems + 1)
 knapsk.DP(solution);
